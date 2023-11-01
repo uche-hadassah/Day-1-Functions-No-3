8c47dbde2258877f233963e8186fa66b7fe56d81
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 15:21:38 +0200

    Cleaned up the code and added useful comments

--- UcheHadassah2_3/HadassahUche2_3.cpp
+++ UcheHadassah2_3/HadassahUche2_3.cpp
@@ -1,32 +1,24 @@
-/*Write a function that computes the sum of digits in an integer. Use the following 
-function header:
-int SumDigits(long n)
-For example, SumDigits(234) returns the value of 2+3+4, i.e. 9 or SumDigits(1347) 
-reurns the value of 1+3+4+7, i.e. 15. Write and test the program with user defined 
-input.
- HINT: Use the % operator to extract a digit and the / operator to remove the 
-extracted digit. For example, to extract 4 from 234 use 234%10 (=4). To remove 4 
-from 234, use 234/10 (=23). Use the loop to repeatedly extract and remove a digit 
-until all digits are extracted.*/
+/*Name:Uche Hadassah
+This program computes the sum of all the digits in an integer*/
 #include<iostream>
 using namespace std;
-int SumDigits(long n);//Function Proyotype
+int SumDigits(long n);//Function Prototype
 int main()
 {
 	long number;
 	cout << "Enter an integer:";
 	cin >> number;
-	cout << "The sum of all the digits in " << number << " is " << SumDigits(number) << endl;
+	cout << "The sum of all the digits in " << number << " is " << SumDigits(number) << endl;//calling the function
 	return 0;
 }
 int SumDigits(long n)
 {
 	int sum = 0;
-	while (n % 10 != 0)
+	while (n % 10 != 0)//while we have not reached the first digit
 	{
-		sum += n % 10;
-		n = n / 10;
+		sum += n % 10;//adds the last digit to sum
+		n = n / 10;// removes the last digit from n
 	}
-	sum += n;
+	sum += n;//adds the remaining number to sum
 	return sum;
 }
